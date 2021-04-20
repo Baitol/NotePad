3bcded1c63071533f76f699a89e943c41a4c2562
Baitol <app.stormer2017@gmail.com>
2021-04-20 11:58:41 +0300

    Notepad v.1.0.0

---
+++ script.js
@@ -0,0 +1,89 @@
+const list = document.getElementById('list');
+const add = document.getElementById('add');
+const del = document.getElementById('del');
+const edit = document.getElementById('edit');
+let checkedBoxes = new Set();
+
+add.addEventListener('click', addElem)
+
+document.addEventListener('keydown', event => {
+  if ((event.key).match(/Enter/)){
+  addElem();
+  }
+})
+
+del.addEventListener('click', function() {
+  checkedBoxes.clear();
+  filtreBoxes();
+  removeElem();
+})
+
+document.addEventListener('keydown', event => {
+  if ((event.key).match(/Delete/)){
+    checkedBoxes.clear();
+    filtreBoxes();
+    removeElem();
+    checkedBoxes = new Set();
+  }
+})
+
+edit.addEventListener('click', function() {
+  checkedBoxes.clear();
+  filtreBoxes();
+  chooseText();
+})
+
+function chooseText() {
+  if (checkedBoxes.size == 1) {
+    //alert("ok");
+    for (let item of checkedBoxes){
+      let text = document.querySelector(`p .${item}`);
+      document.getElementById('inputText').value = text.value;
+    }
+  }else if (checkedBoxes.size == 0) {
+    alert("choose something")
+  }else {
+    alert("to much")
+  }
+}
+
+function addElem() {
+  if (document.getElementById('inputText').value == "") {
+    alert("Input is Empty")
+  } else{
+  let randClass = makeClass();
+  list.insertAdjacentHTML('beforeend', `<p class = '${randClass}'><input class ="${randClass}" type="checkbox"
+                          name="name" value="${document.getElementById('inputText').value}">  ${document.getElementById('inputText').value}</p>`);
+  document.getElementById('inputText').value = "";
+  }
+}
+
+function filtreBoxes() {
+  let elements = list.querySelectorAll('input[type=checkbox]');
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    if (element.checked) {
+      checkedBoxes.add(element.className);
+    }
+  }
+  //for (let value of checkedBoxes) alert(value);
+}
+
+function removeElem() {
+  for (let value of checkedBoxes) {
+    let element = list.querySelectorAll(`.${value}`);
+    for (let variable of element) {
+
+      variable.remove();
+    }
+  }
+}
+
+function makeClass() {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
